07157dc8afd0481e2dcf0deca4effcbedc3db3f0
VictorJThomas <112771636+VictorJThomas@users.noreply.github.com>
2023-08-19 12:32:29 -0400

    Proper new-user routing & Schema fixes

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,7 +1,33 @@
-const NewUser = () => {
-    return (
-        <div>Hi</div>
-    )
-}
+import { currentUser } from '@clerk/nextjs';
+import { prisma } from '../utils/db';
+import { redirect } from 'next/navigation';
+
+const createNewUser = async () => {
+  const user = await currentUser();
+
+  if (user) { // Check if user is not null
+    const match = await prisma.user.findUnique({
+      where: {
+        clerkId: user.id as string,
+      },
+    });
+
+    if (!match) {
+      await prisma.user.create({
+        data: {
+          clerkId: user.id,
+          email: user?.emailAddresses[0]?.emailAddress,
+        },
+      });
+    }
+  }
+
+  redirect('/journal');
+};
+
+const NewUser = async () => {
+  await createNewUser();
+  return <div>...loading</div>;
+};
 
 export default NewUser;
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
 import Link from 'next/link'
+import { auth } from '@clerk/nextjs'
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth()
+
+  let href = userId ? '/journal' : '/new-user'
+
   return (
-    <div className='w-screen h-screen bg-black flex justify-center items-center text-white'>
-      <div className='w-full max-w-[600px] mx-auto'>
-        <h1 className='text-6xl mb-4'>The best journal app & mood tracker</h1>
-        <p className='text-2xl text-white/60 mb-4'>
-          This app is the ultimate tool for monitoring your mood over your lifetime. Simply ensure your honesty for optimal results.
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">The best journal app & mood tracker</h1>
+        <p className="text-2xl text-white/60 mb-4">
+          This app is the ultimate tool for monitoring your mood over your
+          lifetime. Simply ensure your honesty for optimal results.
         </p>
         <div>
-          <Link href="/journal">
-            <button className='bg-blue-600 px-4 py-2 rounded-lg text-xl'>
+          <Link href={href}>
+            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
               get started
             </button>
           </Link>
