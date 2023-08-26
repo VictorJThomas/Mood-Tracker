834da57d010bb6ce6e4c04b365466327216dcc49
VictorJThomas <112771636+VictorJThomas@users.noreply.github.com>
2023-08-26 12:49:55 -0400

    Google Palm AI implementated

--- app/utils/ai.ts
+++ app/utils/ai.ts
@@ -1,8 +1,10 @@
-import {OpenAI} from 'langchain/llms/openai'
-import {} from 'langchain/llms/ollama'
+import { GooglePaLM } from 'langchain/llms/googlepalm'
 
 export const analyze = async (prompt) => {
-    const model = new OpenAI({temperature: 0, modelName: "gpt-3.5-turbo"})
-    const result = await model.call(prompt)
-    console.log(result);
+  const model = new GooglePaLM({
+    temperature:1,
+    modelName: "models/text-bison-001"
+  })
+  const result = await model.call(prompt)
+  console.log(result)
 }
