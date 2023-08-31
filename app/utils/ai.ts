d2d313a96a667b46ac79513afc2f541b55ae9c49
VictorJThomas <112771636+VictorJThomas@users.noreply.github.com>
2023-08-31 07:58:26 -0400

    Zod Schema & Prompt Template

--- app/utils/ai.ts
+++ app/utils/ai.ts
@@ -1,10 +1,49 @@
+import { PromptTemplate } from 'langchain/prompts'
 import { GooglePaLM } from 'langchain/llms/googlepalm'
+import { StructuredOutputParser } from 'langchain/output_parsers'
+import { z } from 'zod'
 
-export const analyze = async (prompt) => {
-  const model = new GooglePaLM({
-    temperature:1,
-    modelName: "models/text-bison-001"
+const parser = StructuredOutputParser.fromZodSchema(
+  z.object({
+    mood: z
+      .string()
+      .describe('the mood of the person who wrote the journal entry.'),
+    summary: z.string().describe('quick summary of the entire entry.'),
+    subject: z.string().describe('the subject of the journal entry.'),
+    negative: z
+      .boolean()
+      .describe(
+        'is the journal entry negative? (i.e. does it contain negative emotions?).'
+      ),
+    color: z
+      .string()
+      .describe(
+        'a hexidecimal color code that represents the mood of the entry. Example #0101fe for blue representing happiness.'
+      ),
   })
-  const result = await model.call(prompt)
+)
+
+const getPrompt = async (content) => {
+  const format_instructions = parser.getFormatInstructions()
+  const prompt = new PromptTemplate({
+    template: 'Analyze the following journal entry. Follow the instructions and format your response to match the format instructions, no matter what! \n{format_instructions}\n{entry}',
+    inputVariables: ['entry'],
+    partialVariables: {format_instructions}
+  })
+
+  const input = await prompt.format({
+    entry: content,
+  })
+
+  return input
+}
+
+export const analyze = async (content) => {
+  const input = await getPrompt(content)
+  const model = new GooglePaLM({
+    temperature: 0,
+    modelName: 'models/text-bison-001',
+  })
+  const result = await model.call(input)
   console.log(result)
 }
