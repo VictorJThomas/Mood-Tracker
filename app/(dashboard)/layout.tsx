e06e712a4f1c21b07208097b11b7fb4c4c41f9e5
VictorJThomas <112771636+VictorJThomas@users.noreply.github.com>
2023-09-08 14:30:02 -0400

    Final

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -9,43 +9,6 @@ const links = [
 ]
 const DashboardLayout = ({ children }) => {
   return (
-    // <div className="w-full h-full relative">
-    //   <aside className="absolute left-0 top-0 h-auto w-[200px] border-r border-black/10">
-    //     <div className="px-4 my-4">
-    //       <span className="text-3xl">MOOD</span>
-    //     </div>
-    //     <ul className="px-4">
-    //       {links.map((link) => (
-    //         <li key={link.href} className="my-4 text-xl">
-    //           <Link href={link.href}>{link.label}</Link>
-    //         </li>
-    //       ))}
-    //     </ul>
-    //   </aside>
-    //   <div className="ml-[200px] h-auto ">
-    //     <header className="h-[60px] border-b border-black/10">
-    //       <nav className="px-4 h-full ">
-    //         <div className="flex items-start justify-start h-full">
-             
-    //         </div>
-    //         <div className="flex items-center justify-end h-full">
-    //           <UserButton
-    //             afterSignOutUrl="/"
-    //             appearance={{
-    //               elements: {
-    //                 userButtonAvatarBox: {
-    //                   width: '3rem',
-    //                   height: '3rem',
-    //                 },
-    //               },
-    //             }}
-    //           />
-    //         </div>
-    //       </nav>
-    //     </header>
-    //     <div className="h-[calc(100vh-60px)]">{children}</div>
-    //   </div>
-    // </div>
     <div className='w-full h-full relative'>
       <div className='ml-[0px] h-auto'>
         <header className="h-[70px] border-b border-black/10">
